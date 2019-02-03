81d17c769f3db4c0b6e80ca40f05424cf9356004
matthew-ferreira <matthew.ferreira@me.com>
2019-02-03 15:27:52 -0500

    Working questions 2-4

--- ECE413HW1_simple/create_chord.m
+++ ECE413HW1_simple/create_chord.m
@@ -24,27 +24,58 @@ function [soundOut] = create_chord( chordType,temperament, root, constants )
 % Constants
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+t = 0:1/constants.fs:constants.durationChord;
+
+switch root
+    case{'A'}
+        rootFreq = 220;
+    case{'B'}
+        rootFreq = 246.9417;
+    case{'C'}
+        rootFreq = 261.6256;
+    case{'D'}
+        rootFreq = 293.6648;
+    case{'E'}
+        rootFreq = 329.6276;
+    case{'F'}
+        rootFreq = 349.2282;
+    case{'G'}
+        rootFreq = 391.9954;
+    otherwise
+        error('Improper root');
+end
 
 switch chordType
     case {'Major','major','M','Maj','maj'}
+        notes = [1 5 8];
     case {'Minor','minor','m','Min','min'}
+        notes = [1 4 8];
     case {'Power','power','pow'}
+        notes = [1 8];
     case {'Sus2','sus2','s2','S2'}
+        notes = [1 3 8];
     case {'Sus4','sus4','s4','S4'}
+        notes = [1 6 8];
     case {'Dom7','dom7','Dominant7', '7'}
+        notes = [1 5 8 11];
     case {'Min7','min7','Minor7', 'm7'}
+        notes = [1 4 8 11];
     otherwise
         error('Inproper chord specified');
 end
 
 switch temperament
-    case {'just','Just'}
     case {'equal','Equal'}
+        freqRatios = [1 2^(1/12) 2^(1/6) 2^(1/4) 2^(1/3) 2^(5/12) 2^(1/2) 2^(7/12) 2^(2/3) 2^(3/4) 2^(5/6) 2^(11/12) 2];
+    case {'just','Just'}
+        freqRatios = [1 16/15 9/8 6/5 5/4 4/3 64/45 3/2 8/5 5/3 9/5 15/8 2];
     otherwise
         error('Inproper temperament specified')
 end
 
 
 % Complete with chord vectors
+sound = sin(2 * pi * (freqRatios(notes) .* rootFreq)' * t);
+soundOut = sum(sound);
 
 end
